5357208bf909af2d34a912c23f8541cc53f8f1dc
Petar Petrov <petar.petrov.georgiev@gmail.com>
2009-06-01 21:42:33 +0000

    * added - concept files & diagrams * added - additional text data * added - technical docs

---
+++ docs/concept/app-concept.mm
@@ -0,0 +1,88 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1225724084680" ID="Freemind_Link_183075212" MODIFIED="1225975100781" TEXT="Brainstorming">
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1225724162383" ID="_" MODIFIED="1225796303343" POSITION="right" TEXT="Contacts View">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1225795876500" ID="Freemind_Link_1912621934" MODIFIED="1229613315042" TEXT="GUI: 3D, Forms  or Skype-alike ?">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1225796146468" ID="Freemind_Link_1925144609" MODIFIED="1229613339323" TEXT="(Contact specific) Mailbox list (list of e-mails)">
+<node CREATED="1225796612546" ID="Freemind_Link_1835507056" MODIFIED="1225796672796" TEXT="Sort by (date,priority)"/>
+<node CREATED="1225974793843" ID="Freemind_Link_1871058914" MODIFIED="1225974801109" TEXT="Show when e-mail was last read"/>
+</node>
+<node CREATED="1225796161546" ID="Freemind_Link_1490546900" MODIFIED="1229613349557" TEXT="(Contact specific) Folders (list of attachments)"/>
+<node CREATED="1225973869890" ID="Freemind_Link_1513369843" MODIFIED="1225974583562" TEXT="User can tag contacts"/>
+<node CREATED="1225975773328" ID="Freemind_Link_41320279" MODIFIED="1225975852265" TEXT="User can enter personal info for the contact (full name,age,other e-mail addresses, etc.)"/>
+</node>
+<node CREATED="1225724169258" ID="Freemind_Link_1381320781" MODIFIED="1225796300937" POSITION="left" TEXT="Tags View">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1225795487328" ID="Freemind_Link_113952204" MODIFIED="1225795502031" TEXT="List 100 tags of most used words within incoming mails">
+<node CREATED="1225974659515" ID="Freemind_Link_7979679" MODIFIED="1225974663375" TEXT="List e-mails upon click"/>
+</node>
+<node CREATED="1225973883875" ID="Freemind_Link_183029116" MODIFIED="1225973890468" TEXT="View personal (contact) tags"/>
+<node CREATED="1225974680609" ID="Freemind_Link_1259342383" MODIFIED="1225974688718" TEXT="View as cloud or list">
+<node CREATED="1225974841421" ID="Freemind_Link_540218195" MODIFIED="1225974843500" TEXT="Cloud ">
+<node CREATED="1225974851984" ID="Freemind_Link_320424591" MODIFIED="1225974868000" TEXT="Show bigger (bolder) view of top tagged words/contacts"/>
+</node>
+<node CREATED="1225974845625" ID="Freemind_Link_360516119" MODIFIED="1225974846734" TEXT="List">
+<node CREATED="1225974824671" ID="Freemind_Link_1216660492" MODIFIED="1225974874859" TEXT="Show last read time "/>
+<node CREATED="1225974695187" ID="Freemind_Link_1827405400" MODIFIED="1229544689671" TEXT="Show number of times used (depth)"/>
+</node>
+</node>
+<node CREATED="1225974736312" ID="Freemind_Link_1666087445" MODIFIED="1225974776140" TEXT="Show FAVOURITE  mails">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1225724179774" ID="Freemind_Link_974969290" MODIFIED="1225796315296" POSITION="right" TEXT="Timespan View">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1225795803265" ID="Freemind_Link_1521772390" MODIFIED="1225795825343" TEXT="Show amounts of e-mail traffics for last 3/6/12 (adjustable) months"/>
+<node CREATED="1225795832234" ID="Freemind_Link_1561290935" MODIFIED="1225795871453" TEXT="Show amounts of e-mail traffic for weeks (week #) and days(?)"/>
+<node CREATED="1225974972187" ID="Freemind_Link_279678916" MODIFIED="1225974981703" TEXT="Use list of calendars to represents months"/>
+<node CREATED="1225974986750" ID="Freemind_Link_1880078133" MODIFIED="1225974998625" TEXT="Use week numbers and year calendar to represent weeks !"/>
+</node>
+<node CREATED="1225726062196" ID="Freemind_Link_1723247703" MODIFIED="1225796301750" POSITION="left" TEXT="Media View">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1225972671093" ID="Freemind_Link_1017907992" MODIFIED="1225972699015" TEXT="Show images, video/audio clips icons like in Windows Explorer. User can browse."/>
+<node CREATED="1225973787937" ID="Freemind_Link_1462675371" MODIFIED="1225973814265" TEXT="Can play/view items (maybe using externals!)">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1225792695531" ID="Freemind_Link_683221795" MODIFIED="1225973915609" POSITION="right" TEXT="Activity Log">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-4"/>
+<node CREATED="1225795430390" ID="Freemind_Link_77763555" MODIFIED="1225795436468" TEXT="Recently wrote/sent mails"/>
+<node CREATED="1225795441578" ID="Freemind_Link_540655382" MODIFIED="1225795452968" TEXT="Top 10 read mails"/>
+<node CREATED="1225795471921" ID="Freemind_Link_1281726541" MODIFIED="1225795479078" TEXT="Top 10 used words in sent mails"/>
+<node CREATED="1225973846156" ID="Freemind_Link_1762324785" MODIFIED="1225973861828" TEXT="Top 10 most contacted persons(contacts)"/>
+</node>
+<node CREATED="1225795103453" ID="Freemind_Link_743262874" MODIFIED="1225973921609" POSITION="left" TEXT="Search">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-5"/>
+<node CREATED="1225795614015" ID="Freemind_Link_1610622790" MODIFIED="1225795620250" TEXT="Search within Tags"/>
+<node CREATED="1225795622437" ID="Freemind_Link_1524787622" MODIFIED="1225795629984" TEXT="Search within IMAP directory"/>
+<node CREATED="1225972711250" ID="Freemind_Link_201914095" MODIFIED="1225975022265" TEXT="Search audio (by length, format or by peeks - loud/quiet)">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1225972758390" ID="Freemind_Link_691620662" MODIFIED="1225975024781" TEXT="Search images (by color, filename, format)">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1225972952921" ID="Freemind_Link_1909391699" MODIFIED="1225975027937" TEXT="Search video (by length, by format, by name)">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1225796659828" ID="Freemind_Link_964606581" MODIFIED="1229525917854" POSITION="right" TEXT="Conversations (History)">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1225971389484" ID="Freemind_Link_445992914" MODIFIED="1225971413078" TEXT="Keep track of e-mails in conversation mode (use [In-Reply] MIME field)"/>
+<node CREATED="1225973017593" ID="Freemind_Link_322907024" MODIFIED="1225973030437" TEXT="Sorted by date of last input"/>
+</node>
+<node CREATED="1225975643406" ID="Freemind_Link_1044766286" MODIFIED="1225975651250" POSITION="left" TEXT="Journal">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1225975651750" ID="Freemind_Link_1238056810" MODIFIED="1225975666265" TEXT="Write a journal and keep it in the mail folder !"/>
+</node>
+</node>
+</map>
