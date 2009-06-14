a1b0d2587fe504b153addc2bca556595dc96a1aa
Petar Petrov <petar.petrov.georgiev@gmail.com>
2009-06-14 19:01:36 +0000

    * added - .net tests solution

---
+++ project/backend/tests/tests.dotnet/soap_test_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Services;
+
+namespace soap_test_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing ...");
+
+
+            localhost.soaplite sp = new localhost.soaplite();
+
+
+            Console.ReadKey();
+        }
+    }
+}
