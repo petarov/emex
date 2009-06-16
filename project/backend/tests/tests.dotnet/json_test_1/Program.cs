b38f8f20e30530a924a9b58fdb25c2449fd38c69
Petar Petrov <petar.petrov.georgiev@gmail.com>
2009-06-16 00:26:59 +0000

    * added - json object files * updated - json c# code. now successfully reading from perl server * updated - perl server code to deliver binary data file as JSON obj

--- project/backend/tests/tests.dotnet/json_test_1/Program.cs
+++ project/backend/tests/tests.dotnet/json_test_1/Program.cs
@@ -8,13 +8,6 @@ using Newtonsoft.Json;
 
 namespace json_test_1
 {
-    class Person
-    {
-        string name;
-        int age;
-        string[] likes;
-    }
-
     class Program
     {
         public string getJSONString(string url)
@@ -34,24 +27,70 @@ namespace json_test_1
             return content;
         }
 
+        public void test_1()
+        {
+            string url = "http://127.0.0.1:8080/json-test-1";
+            Console.WriteLine("Getting from {0}", url);
+            string str = getJSONString(url);
+            Console.WriteLine("JSON String: {0}", str);
+
+            Console.WriteLine("Deserialize into Hash ...");
+            Hashtable h = (Hashtable)JsonConvert.DeserializeObject(str, typeof(Hashtable));
+            Console.WriteLine(h["name"]);
+            Console.WriteLine(h["age"]);
+            Console.WriteLine(h["likes"]);
+
+            Console.WriteLine("Deserialize into Object ...");
+            Person p = (Person)JsonConvert.DeserializeObject(str, typeof(Person));
+            Console.WriteLine(p.name);
+            Console.WriteLine(p.age);
+            Console.WriteLine(p.likes[0]);
+        }
+
+        public void test_2()
+        {
+            string url = "http://127.0.0.1:8080/json-test-2";
+            Console.WriteLine("Getting from {0}", url);
+            string str = getJSONString(url);
+            //Console.WriteLine("JSON String: {0}", str);
+
+            Console.WriteLine("Deserialize into Hash ...");
+            Hashtable h = (Hashtable)JsonConvert.DeserializeObject(str, typeof(Hashtable));
+            Console.WriteLine(h["filename"]);
+            Console.WriteLine(h["size"]);
+            //Console.WriteLine(h["data"]);
+
+            Console.WriteLine("Deserialize into Object ...");
+            BinaryData b = (BinaryData)JsonConvert.DeserializeObject(str, typeof(BinaryData));
+            Console.WriteLine(b.filename);
+            Console.WriteLine(b.size);
+            Console.WriteLine("--JSON LEN: " + b.data.Length);
+            Console.WriteLine("--DECODED FROM B64 - LEN: " + Convert.FromBase64String(b.data).Length);
+
+            // write sample bin file to check agains binary data
+            System.IO.FileStream fs = new System.IO.FileStream(b.filename, System.IO.FileMode.Create);
+            System.IO.BinaryWriter bw = new System.IO.BinaryWriter(fs);
+            //bw.Write(Encoding.ASCII.GetBytes(b.data));
+            bw.Write( Convert.FromBase64String(b.data) );
+            bw.Close();
+            fs.Close();
+            
+        }
+
         public void start()
         {
             Console.WriteLine("Testing ...");
-            string url = "http://127.0.0.1:8080/json-test-1";
-
+            
             try
             {
-                Console.WriteLine("Getting from {0}", url);
-                string str = getJSONString(url);
-
-                Person h = (Person)JavaScriptConvert.DeserializeObject(str, typeof(Person));
-                //Console.WriteLine(h["name"]);
-                Console.WriteLine("OUTPUT: {0}", str);
-                
+                test_1();
+                test_2();
             }
             catch (WebException wex)
             {
                 Console.WriteLine("Web Exception: " + wex.ToString());
+                Console.WriteLine();
+                Console.WriteLine("NOTE: DID YOU START THE PERL SCRIPT HTTP SERVER ? (test.http/server_1.pl)");
             }
 
             Console.WriteLine("Done.");
